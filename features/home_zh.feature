24fb5df2ae120e216fa99cc5045074a0c450c028
蒼時弦也 <elct9620@frost.tw>
2023-01-06 23:40:48 +0800

    Add translated test example

---
+++ features/home_zh.feature
@@ -0,0 +1,10 @@
+#language: zh-TW
+
+功能: 首頁
+  場景: 我可以看到 esbuild 的伺服器
+    當 我打開網址 "/"
+    那麼 我可以看到網頁標題 "Local Server"
+
+  場景: 我可以在伺服器看到 "Hello World"
+    當 我打開網址 "/"
+    那麼 我可以看到文字 "Hello World"
--- features/step_definitions/common.ts
+++ features/step_definitions/common.ts
@@ -6,11 +6,22 @@ When('I visit {string}', async function (this: ExampleWorld, url: string) {
   await this.open(url)
 });
 
+When('我打開網址 {string}', async function (this: ExampleWorld, url: string) {
+  await this.open(url)
+});
 
 Then('I can see the title {string}', async function (this: ExampleWorld, title: string) {
   await expect(this.page).toHaveTitle(title)
 });
 
+Then('我可以看到網頁標題 {string}', async function (this: ExampleWorld, title: string) {
+  await expect(this.page).toHaveTitle(title)
+});
+
 Then('I can see {string}', async function (this: ExampleWorld, text: string) {
+  await expect(this.page.getByText('Hello World')).toBeVisible()
+});
+
+Then('我可以看到文字 {string}', async function (this: ExampleWorld, text: string) {
   await expect(this.page.getByText('Hello World')).toBeVisible()
 });
